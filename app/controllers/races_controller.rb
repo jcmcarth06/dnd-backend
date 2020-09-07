d8ff09d5fb3714c00fe0e9e3c8eabb691bf408a5
jcmcarth06 <jcmcarth06@gmail.com>
2020-09-06 21:41:58 -0600

    add race serializer to races#create

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -7,7 +7,7 @@ class RacesController < ApplicationController
 
     def show 
         race = Race.find_by(id: params[:id])
-        # render RaceSerializer.new(race)
+        render RaceSerializer.new(race)
     end
 
     def create
